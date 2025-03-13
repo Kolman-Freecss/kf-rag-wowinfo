71f64d5b4f315f89e045b0b2a17f7f206d830e3e
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2025-03-13 20:22:26 +0100

    Updated requirements

--- src/kf_rag_wowinfo/main.py
+++ src/kf_rag_wowinfo/main.py
@@ -20,7 +20,7 @@ def main():
     embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
     collection = client.get_or_create_collection("wowinfo", embedding_function=embedding_function)
 
-    # Load data from CSV
+    # Load data from CSV1
     df = pd.read_csv("data/wow_data.csv")
     documents = df['description'].tolist()
     metadatas = df[['class', 'spec']].to_dict('records')
